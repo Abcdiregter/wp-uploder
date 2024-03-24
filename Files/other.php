eb93f696dcf2ad8c9281a8e176ff5c9643c0191d
Abcdiregter <111136892+Abcdiregter@users.noreply.github.com>
2024-03-24 09:51:54 +0700

    Add files via upload

---
+++ Files/other.php
@@ -0,0 +1,3 @@
+<span hidden>theluckyman</span>
+
+// add your code here, dont remove the first line then checker not working
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.2.9 (trial), 000000, 2023-07-11T20:19:11.208026
+from .pyarmor_runtime import __pyarmor__
